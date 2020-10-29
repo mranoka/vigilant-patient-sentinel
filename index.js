4f2d8bd699908034dbbbf961b1ab8e858ee05bfe
mranoka <ranokamahamo@gmail.com>
2020-10-29 12:16:12 +0200

    initial commit2

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ app.use(
 
 // allow Express to serve resources built from React app
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend/build')));
+    app.use(express.static(path.join(__dirname, 'views/build')));
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'views', 'build', 'index.html'));
     });
 }
 
